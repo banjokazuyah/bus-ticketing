d023a2caee2c07bdd53d44718fe14f31aec2c4c7
banjokazuyah <banjokazuyah@gmail.com>
2023-04-22 20:49:31 +0100

    Update contact.vue

--- pages/contact.vue
+++ pages/contact.vue
@@ -6,6 +6,14 @@
     </b-container>
 </template>
 
+<template>
+    <b-container fluid="lg">
+        <b-jumbotron header-level="5" class="my-5" header="Contact Us" lead="Please feel free to contact us using the email address below:">
+            <p><strong>Email:</strong> <a href="mailto:nmb.bus.solutions@gmail.com">nmb.bus.solutions@gmail.com</a></p>
+        </b-jumbotron>
+    </b-container>
+</template>
+
 <script>
 export default {
     layout: "admin",
