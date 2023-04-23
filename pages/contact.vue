5eaa3ac5b1197124ec7c15c2ae1294891972dd76
banjokazuyah <banjokazuyah@gmail.com>
2023-04-23 11:22:49 +0100

    Update contact.vue

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,6 +1,6 @@
 <template>
     <b-container fluid="lg">
-        <b-jumbotron header-level="5" class="my-5" header="Contact Us" lead="Please feel free to contact us using the email address below:">
+        <b-jumbotron header-level="5" class="my-5" header="Contact Us" lead="Please feel free to contact us using details below:">
             <p><strong>Email:</strong> <a href="mailto:nmb.bus.solutions@gmail.com">nmb.bus.solutions@gmail.com</a></p>
             <p><strong>Phone number:</strong> <a href="PhoneNumto:+234 80 980 0000">+234 80 908 0000</a></p>
         </b-jumbotron>
